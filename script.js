7d2db70a7c45212e322405dda276d8fa53c639f2
Yo Yehudi <yo@intermine.org>
2019-05-14 10:40:08 +0100

    input json raw, not just as a file

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
 var demoFile = "http://intermine.org/cytoscape-graph-displayer/example.json",
   form = document.getElementById("myForm"),
   graph = document.getElementById("myGraph"),
-textField = document.getElementById("jsonLink");
+  textField = document.getElementById("jsonLink");
 
 //show graph when the form is submitted
 myForm.addEventListener("submit", function(e) {
   e.preventDefault();
-  var fileToGet = e.target.jsonLink.value;
-  console.log(fileToGet);
-  //grab data from the file
-  $.ajax(fileToGet).done(function(response) {
-    console.log("JSON loaded", response);
-    //tell it what element to attach to
-    response.container = graph;
-    response.layout = {
-      name: "cose"
-    };
-    //put the data inside cytoscape
-    cytoscape(response);
+
+  var ourJson = e.target.jsonLink.value;
+  console.log(ourJson);
+  //check if it's a file or a direct copy/paste
+  jsonType = fileOrJson(ourJson);
+  if(jsonType == "file") {
+    loadFile(ourJson);
+  } else {
+    if (jsonType == "json") {
+      showGraph(JSON.parse(ourJson));
+    }
+  }
+});
+
+function loadFile(ourJson) {
+  //grab data from a remote file
+  $.ajax(ourJson).done(function(response) {
+    showGraph(response);
   }).fail(function(response) {
     //let people know if it goes wrong
     console.error(response);
-    graph.innerHTML = "Error loading " + fileToGet + " " + response.status +
+    graph.innerHTML = "Error loading " + ourJson + " " + response.status +
       " " + response.statusText;
   });
-});
+}
+
+
+
+function fileOrJson(aString) {
+  if (aString.startsWith("http")) {
+    return "file";
+  } else {
+    if (aString.startsWith("{")) {
+      return "json";
+    } else {
+      console.error("neither json nor file")
+      return "error";
+    }
+  }
+}
+
+function showGraph(graphElements){
+  graph.innerHTML = "";
+  console.log("JSON loaded", graphElements);
+  //tell it what element to attach to
+  graphElements.container = graph;
+  graphElements.layout = {
+    name: "cose"
+  };
+  //put the data inside cytoscape
+  cytoscape(graphElements);
+}
+
 
 //show demo graph
 var demo = document.getElementById("demo");
